b42d36f3488f58d8e4495727bb19e06c3a59f0b2
cheybah <cheymabahroun@gmail.com>
2024-02-02 15:48:51 +0100

    added two modules: client and home & completed landing page first version

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,14 +2,37 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage/homepage.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { TopbarComponent } from './topbar/topbar.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { FooterComponent } from './footer/footer.component';
+import { BlogComponent } from './blog/blog.component';
+import { OffersComponent } from './offers/offers.component';
+import { ProcessComponent } from './process/process.component';
+import { PartnersComponent } from './partners/partners.component';
+import { CounterComponent } from './counter/counter.component';
+import { DetailsComponent } from './details/details.component';
+import { PacksComponent } from './packs/packs.component';
 
 
 
 @NgModule({
   declarations: [
-    HomepageComponent
+    HomepageComponent,
+    TopbarComponent,
+    ServicesComponent,
+    AboutusComponent,
+    FooterComponent,
+    BlogComponent,
+    OffersComponent,
+    ProcessComponent,
+    PartnersComponent,
+    CounterComponent,
+    DetailsComponent,
+    PacksComponent
   ],
   exports: [
+    TopbarComponent
   ],
   imports: [
     CommonModule,
--- src/app/home/homepage/homepage.component.ts
+++ src/app/home/homepage/homepage.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit{
 
+  ngOnInit() {
+
+  }
+
+  scrollToTop(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+}
 }
