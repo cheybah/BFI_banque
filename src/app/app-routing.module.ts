ec9e18c4df036ac3668f2b1d3c4e8db641acef06
cheybah <cheymabahroun@gmail.com>
2024-02-06 16:57:32 +0100

    added login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './home/homepage/homepage.component';
 import { LoginComponent } from './client/login/login.component';
+import { SignupComponent } from './client/signup/signup.component';
 
 const routes: Routes = [
   {path: 'home', children:[
@@ -10,6 +11,9 @@ const routes: Routes = [
 
   {path: 'auth', children:[
     {path: 'login',component:LoginComponent},
+    {path: 'signup',component:SignupComponent},
+
+
   ]}
 ];
 
