d79a943b10144fd50eb73e4dfca72ff319a537c1
cheybah <cheymabahroun@gmail.com>
2024-02-06 14:09:46 +0100

    fixed route bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './home/homepage/homepage.component';
+import { LoginComponent } from './client/login/login.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: 'login', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) },
-  { path: '**', redirectTo: '' },
+  {path: 'home', children:[
+    {path: 'homepage',component:HomepageComponent},
+  ]} ,
+
+  {path: 'auth', children:[
+    {path: 'login',component:LoginComponent},
+  ]}
 ];
 
 
