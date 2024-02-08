3d7771f570647cc7a9fd0c0ea23ea8e264eb4343
cheybah <cheymabahroun@gmail.com>
2024-02-08 08:49:55 +0100

    fixed minor styling issues in login and signup pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { HomepageComponent } from './home/homepage/homepage.component';
 import { LoginComponent } from './client/login/login.component';
 import { SignupComponent } from './client/signup/signup.component';
@@ -10,6 +9,7 @@ import { SignupComponent } from './client/signup/signup.component';
 
 const routes: Routes = [
   {path: '', children:[
+    { path: '', redirectTo: '/homepage', pathMatch: 'full' }, // Redirect empty path to '/homepage'
     {path: 'homepage',component:HomepageComponent},
   ]} ,
 
