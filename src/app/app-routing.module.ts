fad2a6818621064c15439b536be89b2038868da2
cheybah <cheymabahroun@gmail.com>
2024-02-14 11:05:23 +0100

    route bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './home/homepage/homepage.component';
 import { LoginComponent } from './client/login/login.component';
 import { SignupComponent } from './client/signup/signup.component';
