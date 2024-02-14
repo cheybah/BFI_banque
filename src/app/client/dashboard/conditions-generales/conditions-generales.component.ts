474cbbc2fd1932e5456bdceda4ff2494cb1490b8
sourour <sourour.methni@gmail.com>
2024-02-14 14:08:38 +0100

    checking generalcond

--- src/app/client/dashboard/conditions-generales/conditions-generales.component.ts
+++ src/app/client/dashboard/conditions-generales/conditions-generales.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./conditions-generales.component.css']
 })
 export class ConditionsGeneralesComponent {
+  conditionsAccepted: boolean = false;
+  formSubmitted: boolean = false;
 
+  submitForm() {
+      this.formSubmitted = true;
+      if (this.conditionsAccepted) {
+          // Soumettre le formulaire ou effectuer d'autres actions
+          console.log("Form submitted successfully!");
+      } else {
+          // Ne rien faire, car le formulaire n'est pas valide
+      }
+  }
 }
