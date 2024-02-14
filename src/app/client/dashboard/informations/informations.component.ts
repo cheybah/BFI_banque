c5dbe28e1165cd399990c38996996f833f7dec4e
cheybah <cheymabahroun@gmail.com>
2024-02-14 10:59:33 +0100

    form control v1

--- src/app/client/dashboard/informations/informations.component.ts
+++ src/app/client/dashboard/informations/informations.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CountryISO } from 'ngx-intl-tel-input';
+import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+
+
+function noNumbersValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const containsNumbers = /\d/.test(control.value);
+    return containsNumbers ? { 'hasNumbers': true } : null;
+  };
+}
 
 @Component({
   selector: 'app-informations',
@@ -8,9 +17,32 @@ import { CountryISO } from 'ngx-intl-tel-input';
   styleUrls: ['./informations.component.css']
 })
 export class InformationsComponent  {
-  constructor(private router: Router) { }
+
+  infoForm: FormGroup;
+
+
+
+  constructor(private router: Router,
+    private formBuilder: FormBuilder
+    ) { 
+    this.infoForm = this.formBuilder.group({
+      genre: [null, Validators.required],
+      firstName: ['', [Validators.required, Validators.pattern('[a-zA-ZÀ-ÿ\s\-]{2,}')]],
+      lastName: ['', [Validators.required, Validators.minLength(2), noNumbersValidator()]],
+      formFile: [''],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required]
+    });
+  }
+
+  
 
   selectedCountryISO: CountryISO = CountryISO.Tunisia; // Initialize with Tunisia as default country
+
+  isFormValid(): boolean {
+    const form = document.querySelector('.needs-validation') as HTMLFormElement;
+    return form.checkValidity();
+  }
   
 
   Retour(){
