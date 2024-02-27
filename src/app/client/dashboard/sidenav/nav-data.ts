a25dcd86cc1aab617f86da29c7a5d134f325aa02
sourour <sourour.methni@gmail.com>
2024-02-27 08:43:12 +0100

    Adjusted transition logic within the sign-in dashboard and added icons to input fields

--- src/app/client/dashboard/sidenav/nav-data.ts
+++ src/app/client/dashboard/sidenav/nav-data.ts
@@ -3,37 +3,45 @@ export  const navbarData =  [
     {
         routeLink : 'informations-personelles',
         icon : 'fal fa-user-circle',
-        label: 'Informations Personnelles'
+        label: 'Informations Personnelles',
+        visited: false // Ajoutez la propriété visited
+
     },
     {
         routeLink : 'adresse',
         icon : 'fal fa-map-marker-alt',
-        label: 'Adresse'
+        label: 'Adresse',
+        visited: false
     },
     {
         routeLink : 'autres-informations',
         icon : 'fal fa-info-circle',
-        label: 'Autres informations'
+        label: 'Autres informations',
+        visited: false
     },
     {
         routeLink : 'offres-et-domicialisation',
         icon : 'fal fa-building',
-        label: 'Offres et domicialisation'
+        label: 'Offres et domicialisation',
+        visited: false
     },
     {
         routeLink : 'conditions-generales',
         icon : 'fal fa-file-alt',
-        label: 'Conditions Génèrales'
+        label: 'Conditions Génèrales',
+        visited: false
     },
     {
         routeLink : 'mot-de-passe',
         icon : 'fa fa-lock',
-        label: 'Mot de passe'
+        label: 'Mot de passe',
+        visited: false
     },
     {
         routeLink : 'validation',
         icon : 'fa fa-check-circle',
-        label: 'Validation'
+        label: 'Validation',
+        visited: false
     },
    
 ]
--- src/app/home/details/details.component.ts
+++ src/app/home/details/details.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  loanAmount: number=0;
+  interestRate: number=5;
+  loanTerm: number=0;
+  monthlyPayment: number=0;
 
+  
+
+  calculatePayment() {
+    const loanAmount=this.loanAmount;
+    const annualInterestRate = this.interestRate / 100; // Convertir le taux d'intérêt annuel en décimal
+    const monthlyInterestRate = annualInterestRate / 12; // Calculer le taux d'intérêt mensuel
+    const numberOfPayments = this.loanTerm * 12; // Calculer le nombre total de paiements
+    // Calcul du paiement mensuel selon la formule fournie
+    const monthlyPayment = (loanAmount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
+    this.monthlyPayment = monthlyPayment;
+  }
 }
