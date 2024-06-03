bf2ce003cdab81485e2f7e7003b7d27763a4306b
sourour <sourour.methni@gmail.com>
2024-06-03 11:59:27 +0200

    clearing local storage

--- src/app/client/transactionsDashboard/default-layout/navbar/navbar.component.ts
+++ src/app/client/transactionsDashboard/default-layout/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   page = "Page Name"
+  constructor(private router: Router) { }
+  logOut(): void {
+    localStorage.clear;
+    this.router.navigate(['/bfi/login']);
+  }
 }
